a94f6fa1b42e394b1c6bbcde6f4923f63671eb53
Matthew Lean <matt@mattlean.com>
2017-12-15 22:55:45 -0800

    Refactor config

--- config/index.js
+++ config/index.js
@@ -3,9 +3,13 @@
 const _ = require('lodash');
 
 const config = {
-  MONGODB_CONNECTION_STR: `mongodb://localhost:27017/lean-express-mongo-starter`,
-  PORT: process.env.PORT || 3000
+  DB_NAME: process.env.DB_NAME || 'lean-express-mongo-starter',
+  PORT: process.env.PORT || 3000,
+  MONGO_PORT: process.env.MONGO_PORT || 27017,
+  MONGO_HOST: process.env.MONGO_HOST || 'localhost'
 };
+
+config.MONGO_URI = process.env.MONGO_URI || `mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.DB_NAME}`;
 
 config.ENV = process.env.NODE_ENV;
 
