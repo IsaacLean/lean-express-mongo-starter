caf7ab228995b7f8b112a87f65eec24f9b8d2b31
Matthew Lean <matt@mattlean.com>
2017-12-16 00:37:19 -0800

    Use minified dependencies for template test, run npm start with production flag

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const router = express.Router();
 
+const ENV = require('../config').ENV;
+
 router.get('/', (req, res) => {
   res.render('root', { headTitle: 'Lean Express/Mongo Starter' });
 });
@@ -10,7 +12,7 @@ router.get('/', (req, res) => {
 router.get('/hello-world', (req, res) => res.send('Hello World!'));
 
 router.get('/template-test', (req, res) => {
-  res.render('template-test', {
+  const locals = {
     content: `A big strong tree needs big strong roots.
       There isn't a rule.
       You just practice and find out which way works best for you.
@@ -19,10 +21,18 @@ router.get('/template-test', (req, res) => {
       There are no limits in this world.
       Go out on a limb - that's where the fruit is.`,
     contentTitle: 'Happy Trees',
-    css: ['/static/css/bootstrap.css', '/static/style.css'],
-    headTitle: 'Template Test',
-    scripts: ['/static/jquery.js', '/static/popper.js', '/static/js/bootstrap.js']
-  });
+    headTitle: 'Template Test'
+  };
+
+  if (ENV === 'production') {
+    locals.css = ['/static/css/bootstrap.min.css', '/static/style.css'];
+    locals.scripts = ['/static/jquery.min.js', '/static/popper.min.js', '/static/js/bootstrap.min.js'];
+  } else {
+    locals.css = ['/static/css/bootstrap.css', '/static/style.css'];
+    locals.scripts = ['/static/jquery.js', '/static/popper.js', '/static/js/bootstrap.js'];
+  }
+
+  res.render('template-test', locals);
 });
 
 module.exports = router;
